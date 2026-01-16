e6ad6c33c77a480becfd49d635cf72ac385c3c30
Michelangelo Setaro <mksetaro@gmail.com>
2026-01-16 16:46:17 +0000

    runs local code

---
+++ loss_vegas/main.py
@@ -0,0 +1,5 @@
+from trainer import Trainer
+
+
+def main():
+    trainer = Trainer()
---
+++ loss_vegas/trainer.py
@@ -0,0 +1,23 @@
+import torch.nn as nn
+import torch.optim as optim
+import torch.utils.data as data
+from lightning.fabric import Fabric
+
+
+class Trainer:
+    def __init__(
+        self,
+        model: nn.Module = None,
+        optimizer: optim.Optimizer = None,
+        data_loader: data.DataLoader = None,
+    ):
+        self.fabric = Fabric()
+        if self.fabric.world_size > 1:
+            raise NotImplementedError("Distributed training is not supported yet.")
+        # self.model = self.fabric.setup_module(model)
+        # self.optimizer = self.fabric.setup_optimizers(optimizer)
+        # self.data_loader = self.fabric.setup_dataloaders(data_loader)
+
+    def train(self):
+        # Placeholder for training logic
+        pass
